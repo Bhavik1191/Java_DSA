5f57e96c1f5875bbc1118f35eb033aef342bca39
Bhavik1191 <bhavikkshah91@gmail.com>
2021-11-08 16:58:42 +0530

    floorNumber

--- src/main/java/searchAlgos/binarySearch/CeilingNumber.java
+++ src/main/java/searchAlgos/binarySearch/CeilingNumber.java
@@ -2,9 +2,17 @@ package searchAlgos.binarySearch;
 
 public class CeilingNumber {
 
+    /**
+     *
+     * Ceiling number - Smallest number in the array which is greater than or equal to the Target number
+     * Floor number - Highest number in the array which is smaller than or equal to Target number
+     * @param args
+     */
     public static void main(String[] args) {
-        int[] arr = {1,4,7,9,13,14,16,18};
-        int target = 17;
+
+        int[] arr = {2,4,7,9,13,14,16,18};
+        int target = 0;
+
 
         System.out.println("Ceiling of the given number is : "+ getCeilingNumber(arr, target,0,arr.length-1 ));
     }
@@ -14,13 +22,17 @@ public class CeilingNumber {
         int middle = 0;
         int last = 0;
 
+        //what if target is greater than greatest number in array
+        if(target>arr[arr.length-1])
+            return -1;
+
+
         while (start <= end)
         {
             middle = end-start/2;
             if(target>arr[middle])
             {
                 start = middle+1;
-                last = arr[start];
             }
             else if(target==arr[middle])
             {
@@ -29,7 +41,6 @@ public class CeilingNumber {
             else
             {
                 end = middle-1;
-                last = arr[middle];
             }
         }
         return arr[start];
---
+++ src/main/java/searchAlgos/binarySearch/FloorNumbers.java
@@ -0,0 +1,40 @@
+package searchAlgos.binarySearch;
+
+public class FloorNumbers {
+
+        public static void main(String[] args) {
+
+            int[] arr = {2,4,7,9,13,14,16,18};
+            int target = 1;
+
+
+            System.out.println("Ceiling of the given number is : "+ getFloorNumber(arr, target,0,arr.length-1 ));
+        }
+
+        private static int getFloorNumber(int[] arr, int target,int start, int end) {
+
+            int middle = 0;
+
+            //what if target is lesser than least number in array
+            if(target<arr[0])
+                return -1;
+
+            while (start <= end)
+            {
+                middle = end-start/2;
+                if(target>arr[middle])
+                {
+                    start = middle+1;
+                }
+                else if(target==arr[middle])
+                {
+                    return arr[middle];
+                }
+                else
+                {
+                    end = middle-1;
+                }
+            }
+            return arr[end];
+        }
+}
