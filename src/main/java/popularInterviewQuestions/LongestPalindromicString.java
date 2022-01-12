2813d83f3a35f714e7babae6411a79ee3672a343
Bhavik1191 <bhavikkshah91@gmail.com>
2022-01-12 16:08:17 +0530

    longest palindrom with expan from middle

--- src/main/java/popularInterviewQuestions/LongestPalindromicString.java
+++ src/main/java/popularInterviewQuestions/LongestPalindromicString.java
@@ -5,9 +5,53 @@ public class LongestPalindromicString {
     //https://leetcode.com/problems/longest-palindromic-substring/
     public static void main(String[] args) {
 
-        String s = "cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj";
+        String s = "lddlfgffgfld";
         String ans = longestPalindrome(s);
-        System.out.println("Final ans : " +ans);
+        System.out.println("Final ans : " +longestPalindrome(s));
+        System.out.println("Final ans with Leetecode ans : " +longesWithLeeteCodeAnswer(s));
+    }
+
+    public static String longesWithLeeteCodeAnswer(String s)
+    {
+
+        if(s.length()<1 || s ==null)
+            return s;
+
+        int start = 0;
+        int end = 0;
+
+        for(int i =0 ; i<s.length(); i++)
+        {
+
+
+            int len1 = expandFromMiddle(s,i,i);
+            int len2 = expandFromMiddle(s,i,i+1);
+            int len = Math.max(len1, len2);
+
+            if(len > end-start)
+            {
+                start = i - (len-1)/2;
+                end = i + len/2;
+            }
+
+        }
+
+        return s.substring(start, end+1);
+
+    }
+
+    public static int expandFromMiddle(String s, int left, int right)
+    {
+
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+        {
+            left--;
+            right++;
+        }
+
+        return right - left -1;
+
     }
 
     private static String longestPalindrome(String s) {
